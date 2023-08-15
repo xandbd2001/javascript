d448aa0386078c1b1d39061d98a0789f8b93ebe1
Alexandre Pereira Brandão <alexandrepb51@hotmail.com>
2023-08-14 22:08:45 -0300

    Atualização aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/*var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+/*
+
+/*
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+*/
