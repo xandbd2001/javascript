8faf4e72227a137d5f0607dcd6bb101cfcb09c73
Alexandre Pereira Brandão <alexandrepb51@hotmail.com>
2023-09-18 21:15:43 -0300

    Atualização de Aula

---
+++ aula 15/ambiente.js
@@ -0,0 +1,9 @@
+var num = [5,8,2,9,3]
+num [5] = 10
+num.push(11)
+//num.length (Saber quantos elementos o vetor possui)
+// num.sort() Ordem Crescente
+
+console.log(`Nosso Vetor é o: ${num}`)
+console.log(`Nosso Vetor Possui ${num.length} Elementos!`)
+console.log(`O Primeiro Elemento do Vetor é: ${num[0]}`)
