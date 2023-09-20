619bd6c7e4b3e69ef13167dd40b48c3ae2474ef1
Alexandre Pereira Brandão <alexandrepb51@hotmail.com>
2023-09-19 21:11:47 -0300

    Atualização de Aula

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -7,3 +7,9 @@ num.push(11)
 console.log(`Nosso Vetor é o: ${num}`)
 console.log(`Nosso Vetor Possui ${num.length} Elementos!`)
 console.log(`O Primeiro Elemento do Vetor é: ${num[0]}`)
+let pos = num.indexOf(15)
+if (pos == -1) {
+    console.log('O Valor Não foi encontrado')
+} else {
+    console.log(`O Valor 8 esta na posição ${pos}`)
+}
