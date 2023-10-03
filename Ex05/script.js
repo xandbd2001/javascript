bfd48fef55f7bb17d082e99de29ad6856945cf97
Alexandre Pereira Brandão <alexandrepb51@hotmail.com>
2023-10-02 21:03:09 -0300

    Analisador de Números

--- Ex05/script.js
+++ Ex05/script.js
@@ -21,8 +21,40 @@ function inLista(n,l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value,valores) ) {
-        window.alert('Tudo Ok!')
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na Lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for(var pos in valores) {
+            soma += valores [pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]        
+        }
+        media = soma / tot        
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} númeors cadastrados.</p>`
+        res.innerHTML += `<p>O Maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O Menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A Média dos valores digitados é ${media}.</p>`
     }
 }
